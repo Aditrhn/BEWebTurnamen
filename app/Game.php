feb6f7ff74fa4c507191fb28941c493399adb38f
WahidinAji <a17wahidin@gmail.com>
2020-08-23 11:37:36 +0700

    filable to fillable

--- app/Game.php
+++ app/Game.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    protected $filable = ['name', 'platform'];
+    protected $fillable = ['name', 'platform'];
     public $timestamps = \true;
 }
