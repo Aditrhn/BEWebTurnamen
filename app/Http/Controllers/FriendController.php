f5f6b3e5fab7b2dd79448f44d1bdd1e5fc701cca
Aditrhn <aditiya.rahman9b@gmail.com>
2020-09-29 00:45:51 +0700

    Friend system
    Add FriendController, SearchController, friend page, search result page, search routing, and friend routing

---
+++ app/Http/Controllers/FriendController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Player;
+use App\Model\Friend;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class FriendController extends Controller
+{
+    public function index(Request $request)
+    {
+        $player_id = Auth::guard('player')->user()->id;
+        // $friendlists = DB::select('select * from friends f join players p on p.id = f.player_two where f.status = ?', ["1"]);
+        $friendlists = DB::select('select * from friends f join players p on p.id = f.player_one or p.id = player_two 
+        where not p.id = '.$player_id.' and (f.player_one = '.$player_id.' or f.player_two = '.$player_id.') and f.status = "1"');
+        $friend_requests = DB::select('select * from friends f join players p on p.id = f.player_one where f.status = ? and f.player_two = ?', ["0",$player_id]);
+
+        return \view('friend.index', \compact('friendlists','friend_requests'));
+    }
+    public function add_friend(Request $request)
+    {
+        if ($request->has('id')) {
+
+            $player_id = Auth::guard('player')->user()->id;
+            $friend_id = $request->id;
+
+            $friend = DB::insert('insert into friends (player_one, player_two, status) values ('.$player_id.', '.$friend_id.', "0")');
+
+            return redirect()->back();
+        }
+    }
+    public function accept_friend(Request $request)
+    {
+        if($request->has('accept')) {
+            $player_id = Auth::guard('player')->user()->id;
+            $friend_id = $request->accept;
+
+            $accept_friend = DB::update('update friends set status = "1" where player_one = '.$friend_id.' and player_two = '.$player_id);
+
+            return redirect()->back();
+        }
+    }
+    public function decline_friend(Request $request){
+        if ($request->has('decline')) {
+
+            $player_id = Auth::guard('player')->user()->id;
+            $friend_id = $request->decline;
+
+            $decline_friends = DB::delete('delete from friends where player_one = '.$friend_id.' and player_two = '.$player_id);
+
+            return redirect()->back();
+        }
+    }
+    public function unfriend(Request $request){
+        if ($request->has('unfriend')) {
+
+            $player_id = Auth::guard('player')->user()->id;
+            $friend_id = $request->unfriend;
+
+            $unfriends = DB::delete('delete from friends where player_one = '.$friend_id.' and player_two = '.$player_id);
+
+            return redirect()->back();
+        }
+    }
+}
---
+++ resources/views/friend/index.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.main')
+@section('asset-toastr')
+<link rel="stylesheet" href="assets/vendor/toastr/toastr.min.css">
+@endsection
+@section('main')
+<!-- MAIN -->
+<div class="main">
+  <!-- MAIN CONTENT -->
+  <div class="main-content">
+    <!-- Container -->
+    <div class="container-fluid">
+      <div class="col-md-12">
+        <ul class="nav nav-pills marginPils">
+          <li class="active pillsFriend"><a data-toggle="pill" href="#friendList">Friend List</a></li>
+          <li class="pillsRequest"><a data-toggle="pill" href="#menu1" class="pillsFriend">Friend Request</a></li>
+        </ul>
+      </div>
+      <!--Konten Pills-->
+      <div class="tab-content tabKonten">
+        <div id="friendList" class="tab-pane fade in active">
+          <div class="row">
+            @forelse ($friendlists as $item)
+            <div class="col-md-3 friend-page">
+              <div class="panel panel-headline panel-friend-detail">
+                <div class="panel-body">
+                    <img class="img-panel-friend" src="assets/img/user3.png">
+                    <h4 class="panel-friend">{{ $item->name }}</h4>
+                    <form action="{{ URL::route('unfriend') }}" method="POST">
+                      @csrf
+                      <div class="buttons col-md-12 btnAdd">
+                        <input type="hidden" name="unfriend" value="{{ $item->id }}">
+                        <button class="btn btn-xs btn-primary" id="btnUnfriend" type="submit">Unfriend</button>
+                      </div>
+                    </form>
+                </div>
+              </div>
+            </div>
+            @empty
+            <div class="panel-friend not-found">
+              <h4>Anda masih belum memiliki teman</h4>
+            </div>
+            @endforelse
+          </div>
+          <!-- End Panel Friend List -->
+        </div>
+
+        <div id="menu1" class="tab-pane fade">
+          <div class="row">
+            @forelse ($friend_requests as $item)
+            <div class="col-md-3 friend-page">
+              <div class="panel panel-headline panel-friend-detail">
+                <div class="panel-body">
+                  <img class="img-panel-friend" src="assets/img/user3.png">
+                  <h4 class="panel-friend" data-toggle="modal">{{ $item->name }}</h4>
+                  <form action="{{ URL::route('accept-friend') }}" method="POST">
+                    @csrf
+                    <div class="buttons col-md-6 btnRquest">
+                      <input type="hidden" name="accept" value="{{ $item->id }}">
+                      <button class="btn btn-xs btn-primary" id="btnAccept" type="submit">Accept</button>
+                    </div>
+                  </form>
+                  <form action="{{ URL::route('decline-friend') }}" method="POST">
+                    @csrf
+                    <div class="buttons col-md-6 btnRquest">
+                      <input type="hidden" name="decline" value="{{ $item->id }}">
+                      <button class="btn btn-xs btn-unfriend" id="btnUnfriend" type="submit">Decline</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            @empty
+            <div class="panel-friend not-found">
+              <h4>Belum ada permintaan pertemanan.</h4>
+            </div>
+            @endforelse
+          </div>
+        </div>
+      </div>
+      <!--End Konten Pills-->
+    </div>
+    <!-- End Container -->
+  </div>
+  <!-- END MAIN CONTENT -->
+</div>
+<!-- END MAIN -->
+@endsection
