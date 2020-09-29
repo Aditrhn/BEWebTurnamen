4a235ca5c31f2ceb1c27557a68c6d871b51b9e85
Aditrhn <aditiya.rahman9b@gmail.com>
2020-09-29 23:41:43 +0700

    Update
    Index dan css

--- resources/views/friend/index.blade.php
+++ resources/views/friend/index.blade.php
@@ -5,346 +5,83 @@
 @section('main')
 <!-- MAIN -->
 <div class="main">
-			<!-- MAIN CONTENT -->
-			<div class="main-content">
-				<!-- Container -->
-				<div class="container-fluid">
-					<div class="col-md-12">
-						<ul class="nav nav-pills marginPils">
-							<li class="active pillsFriend"><a data-toggle="pill" href="#friendList">Friend List</a></li>
-							<li class="pillsRequest"><a data-toggle="pill" href="#menu1" class="pillsFriend">Friend Request</a></li>
-						</ul>
-					</div>
-					<!--Konten Pills-->
-					<div class="tab-content tabKonten">
-						<div id="friendList" class="tab-pane fade in active">
-							<div class="row">
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Atra</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Mystea</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">TehBotol</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Mang Ole</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!--Baris 2-->
-							<div class="row">
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Odading</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Iron-men</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">IkanHui</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Makan</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!--Baris 3-->
-							<div class="row">
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Tomat</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Goblog</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Odading</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-												<img class="img-panel-friend" src="assets/img/user3.png">
-												<h4 class="panel-friend">Rasanya</h4>
-												<div class="buttons col-md-12 btnAdd">
-													<button class="btn btn-xs btn-primary" id="btnUnfriend" href="#">Unfriend</button>
-												</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!-- End Panel Friend List -->
-						</div>
+  <!-- MAIN CONTENT -->
+  <div class="main-content">
+    <!-- Container -->
+    <div class="container-fluid">
+      <div class="col-md-12">
+        <ul class="nav nav-pills marginPils">
+          <li class="active pillsFriend"><a data-toggle="pill" href="#friendList">Friend List</a></li>
+          <li class="pillsRequest"><a data-toggle="pill" href="#menu1" class="pillsFriend">Friend Request</a></li>
+        </ul>
+      </div>
+      <!--Konten Pills-->
+      <div class="tab-content tabKonten">
+        <div id="friendList" class="tab-pane fade in active">
+          <div class="row">
+            @forelse ($friendlists as $item)
+            <div class="col-md-3 friend-page">
+              <div class="panel panel-headline panel-friend-detail">
+                <div class="panel-body">
+                    <img class="img-panel-friend" src="assets/img/user3.png">
+                    <h4 class="panel-friend">{{ $item->name }}</h4>
+                    <form action="{{ URL::route('unfriend') }}" method="POST">
+                      @csrf
+                      <div class="buttons col-md-12 btnAdd">
+                        <input type="hidden" name="unfriend" value="{{ $item->id }}">
+                        <button class="btn btn-xs btn-primary" id="btnUnfriend" type="submit">Unfriend</button>
+                      </div>
+                    </form>
+                </div>
+              </div>
+            </div>
+            @empty
+            <div class="panel-friend not-found">
+              <h4>Anda masih belum memiliki teman</h4>
+            </div>
+            @endforelse
+          </div>
+          <!-- End Panel Friend List -->
+        </div>
 
-						<div id="menu1" class="tab-pane fade">
-							<div class="row">
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend" data-toggle="modal">Mystea</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend" data-toggle="modal">Atra</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">RRQ-Maniac</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend" data-toggle="modal">TehBotol</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!--Baris 2-->
-							<div class="row">
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend" data-toggle="modal">Mystea</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">Atra</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">RRQ-Maniac</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">TehBotol</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!--Baris 3-->
-							<div class="row">
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">Mystea</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">Atra</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">RRQ-Maniac</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3 friend-page">
-									<div class="panel panel-headline panel-friend-detail">
-										<div class="panel-body">
-											<img class="img-panel-friend" src="assets/img/user3.png">
-											<h4 class="panel-friend">TehBotol</h4>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-primary" id="btnAccept" href="#">Accept</button>
-											</div>
-											<div class="buttons col-md-6 btnRquest">
-												<button class="btn btn-xs btn-unfriend" id="btnUnfriend" href="#">Decline</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!--End Konten Pills-->
-				</div>
-				<!-- End Container -->
-			</div>
-			<!-- END MAIN CONTENT -->
-		</div>
-		<!-- END MAIN -->
+        <div id="menu1" class="tab-pane fade">
+          <div class="row">
+            @forelse ($friend_requests as $item)
+            <div class="col-md-3 friend-page">
+              <div class="panel panel-headline panel-friend-detail">
+                <div class="panel-body">
+                  <img class="img-panel-friend" src="assets/img/user3.png">
+                  <h4 class="panel-friend" data-toggle="modal">{{ $item->name }}</h4>
+                  <form action="{{ URL::route('accept-friend') }}" method="POST">
+                    @csrf
+                    <div class="buttons col-md-6 btnRquest">
+                      <input type="hidden" name="accept" value="{{ $item->id }}">
+                      <button class="btn btn-xs btn-primary" id="btnAccept" type="submit">Accept</button>
+                    </div>
+                  </form>
+                  <form action="{{ URL::route('decline-friend') }}" method="POST">
+                    @csrf
+                    <div class="buttons col-md-6 btnRquest">
+                      <input type="hidden" name="decline" value="{{ $item->id }}">
+                      <button class="btn btn-xs btn-unfriend" id="btnUnfriend" type="submit">Decline</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            @empty
+            <div class="panel-friend not-found">
+              <h4>Belum ada permintaan pertemanan.</h4>
+            </div>
+            @endforelse
+          </div>
+        </div>
+      </div>
+      <!--End Konten Pills-->
+    </div>
+    <!-- End Container -->
+  </div>
+  <!-- END MAIN CONTENT -->
+</div>
+<!-- END MAIN -->
 @endsection
