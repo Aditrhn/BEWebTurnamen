f294ccf88b387400c675b8cbc9995a21a30e5da9
WahidinAji <a17wahidin@gmail.com>
2020-10-03 02:10:34 +0700

    fixed store logo team

--- app/Http/Controllers/Player/TeamController.php
+++ app/Http/Controllers/Player/TeamController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\Player;
 
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-class TeamController extends Controller 
+class TeamController extends Controller
 {
     public function index()
     {
@@ -18,14 +18,15 @@ class TeamController extends Controller
     }
     public function team_create_page()
     {
-        $games = Game::select('name', 'id')->get();
+        // $games = Game::select('name', 'id')->get();
 
-        return \view('team.create', \compact('games'));
+        // return \view('team.create', \compact('games'));
+        return \view('team.create');
     }
-    public function team_create(Request $request)
+    public function store(Request $request)
     {
         // $games = Game::select('name', 'id')->get();
-        $games_id = Game::where('name', 'LIKE', '%' . $request->teamGame . '%')->first();
+        // $games_id = Game::where('name', 'LIKE', '%' . $request->teamGame . '%')->first();
         // $check = Team::where('name', 'LIKE', '%' . $request->teamName . '%')->first();
 
         // if ($check == null) {
@@ -44,21 +45,21 @@ class TeamController extends Controller
 
         $this->validate($request, [
             'name' => 'required',
-            'games_id' => 'required',
+            // 'games_id' => 'required',
         ]);
         $file = $request->file('logo_url');
         $name_icon = \time() . "_" . $file->getClientOriginalName();
         $tujuan_upload = 'images/team_logo/';
         $file->move($tujuan_upload, $name_icon);
         Team::create([
-            'name' => $request->teamName,
-            'max_member' => 5,
-            'logo_url' => $name_icon,
-            'games_id' => $games_id->id
+            'name' => $request->name,
+            'max_member' => 10,
+            'logo_url' => $name_icon
+            // 'games_id' => $games_id->id
         ]);
 
         dd($name_icon);
-        return \redirect('team.overview')->with(['success' => 'Team created successfully']);
+        // return \redirect('team-overview')->with(['success' => 'Team created successfully']);
     }
     public function team_overview()
     {
@@ -69,4 +70,3 @@ class TeamController extends Controller
         return \view('team.search');
     }
 }
-?>
--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -5,17 +5,18 @@
     <!-- MAIN CONTENT -->
     <div class="main-content">
         <div class="container-fluid">
-            <form action="{{ URL::route('team-create-success') }}" method="POST">
+            <form action="{{ URL::route('team.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
+                {{ csrf_token() }}
                 <div class="row" id="rowcreate">
                     <div class="col-md-6">
                         <div class="form-group" id="scrollform-teamcreate">
-                            <label for="exampleInputEmail1">Team Name</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" name="teamName">
+                            <label for="name">Team Name</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="exampleInputEmail1" name="name">
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <label for="upload_avatar" id="avatar"> Team Logo (optional)</label>
+                        <label for="logo_url" id="avatar"> Team Logo (optional)</label>
                         <div class="input-group" id="upload_logo_team">
                             <div class="custom-file" id="browse">
                                 <input type="file" class="custom-file-input @error('ava_url') is-invalid @enderror"
@@ -29,13 +30,13 @@
                 </div>
                 <label for="scrollform" id="scrollform-teamcreate">Game</label>
                 <div id="custom-select">
-                    <select name="teamGame">
+                    {{-- <select name="teamGame">
                         @forelse($games as $item)
                             <option>{{ $item->name }}</option>
                         @empty
                             <p>Belum ada game yang bisa dipilih.</p>
                         @endforelse
-                    </select>
+                    </select> --}}
                 </div>
                 <div class="form-group" id="formgroup-teamcreate">
                     <label for="exampleInputEmail1" id="scrollform-teamcreate">Description</label>
