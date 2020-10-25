27d2a81b016cda0982e1f97f570f6202cdf0623a
WahidinAji <a17wahidin@gmail.com>
2020-10-25 16:10:12 +0700

    add default string lenght for migration db 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(256);
+        Schema::defaultStringLength(191);
     }
 }
