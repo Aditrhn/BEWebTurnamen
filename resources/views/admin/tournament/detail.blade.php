04a966eb5bcad73ff4bf0f80a6cad7b251e86c71
WahidinAji <a17wahidin@gmail.com>
2020-10-26 19:28:12 +0700

    add btn create matches

--- resources/views/admin/tournament/detail.blade.php
+++ resources/views/admin/tournament/detail.blade.php
@@ -186,6 +186,7 @@
                                 <div class="row">
                                 <div class="col-md-12">
                                     <h3 class="titleMatch">Match List</h3>
+                                    <a href="{{ URL::route('match.create') }}" class="badge badge-primary">Create</a>
                                     <h4 class="titleMatch">Round 1</h4>
                                 </div>
                                 <div class="col-md-12">
