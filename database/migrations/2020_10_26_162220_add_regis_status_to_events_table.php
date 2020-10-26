c8d7555e99c811c9e89bee981183689bb91a4d68
Makrovic <44518143+Makrovic@users.noreply.github.com>
2020-10-26 23:28:12 +0700

    add column regis status on events

---
+++ database/migrations/2020_10_26_162220_add_regis_status_to_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRegisStatusToEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->enum('regis_status',['0','1'])->default('0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+}
