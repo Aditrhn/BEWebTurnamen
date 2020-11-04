08e5c7c479fb87e1c50b5839984fb5e402c13d48
KukuhSetyoB <41356066+KukuhSetyoB@users.noreply.github.com>
2020-11-04 21:01:18 +0700

    activasi carousel pada dashboard

--- resources/views/player/dashboard.blade.php
+++ resources/views/player/dashboard.blade.php
@@ -22,7 +22,7 @@
 								<h3 class="head-text">
 									Tournament By Game</h3>
 							</div>
-							<!-- Control
+							<!-- Control-->
 
 							<div class="col-md-3">
 								<div class="controls team-control-btn pull-right">
@@ -32,9 +32,9 @@
 											data-slide="next"></a>
 								</div>
 
-							</div> -->
+							</div> 
 						</div>
-						<div class="col-md-12"><!--Class utama Carousel "carousel-landing"-->
+						<div class="col-md-12 carousel-landing"><!--Class utama Carousel "carousel-landing"-->
 							<div id="Carousel" class="carousel slide">
 							<!-- Carousel items -->
 								<div class="carousel-inner">
@@ -55,30 +55,29 @@
 													   <h2 class="case-landing__title">PUBG</h2>
 													</div>
 												</div>
-
 										</div><!--.row-->
 									</div>
-									<!--
+									
 									<div class="item">
-										<div class="row">
-											<div class="case-landing landing1">
-												<div class="case-landing__overlay">
-													<h2 class="case-landing__title">Apex Legends</h2>
+										<div class="row rowDashboard">
+												<div class="case-landing landing1" style="background-image: url('https://duniaesports.org/wp-content/uploads/2019/12/t_5dd64b08483f2.jpg');">
+													<div class="case-landing__overlay">
+													   <h2 class="case-landing__title">Mobile Legend</h2>
+													</div>
 												</div>
-											</div>
-											<div class="case-landing landing1">
-												<div class="case-landing__overlay">
-													<h2 class="case-landing__title">Apex Legends</h2>
+												<div class="case-landing landing1" style="background-image: url('https://id-test-11.slatic.net/p/88cd68df4f9451f7febafdc3543459e6.png');">
+													<div class="case-landing__overlay">
+													   <h2 class="case-landing__title">Free Fire</h2>
+													</div>
 												</div>
-											</div>
-											<div class="case-landing landing1">
-												<div class="case-landing__overlay">
-													<h2 class="case-landing__title">Apex Legends</h2>
+												<div class="case-landing landing1" style="background-image: url('https://pbs.twimg.com/profile_images/1083705443132690433/6j9H0Dxl_400x400.jpg');">
+													<div class="case-landing__overlay">
+													   <h2 class="case-landing__title">PUBG</h2>
+													</div>
 												</div>
-											</div>
-										</div>
+										</div><!--.row-->
 									</div>
-									-->
+									
 								</div>
 							</div>
 						</div>
@@ -92,7 +91,7 @@
 								<h3 class="head-text">
 									Featured Tournament</h3>
 							</div>
-							<!-- Controls Carousel
+							<!-- Controls Carousel-->
 							<div class="col-md-3">
 								<div class="controls team-control-btn pull-right">
 									<a class="left glyphicon glyphicon-triangle-left team_columns_carousel_control_icons" href="#Carousel-Feature"
@@ -100,9 +99,9 @@
 									<a class="right glyphicon glyphicon-triangle-right team_columns_carousel_control_icons" href="#Carousel-Feature"
 											data-slide="next"></a>
 								</div>
-							</div> -->
+							</div> 
 						</div>
-						<div class="col-md-12"> <!--Class utama Carousel "carousel-landing"-->
+						<div class="col-md-12 carousel-landing"> <!--Class utama Carousel "carousel-landing"-->
 							<div id="Carousel-Feature" class="carousel slide">
 							<!-- Carousel items -->
 								<div class="carousel-inner">
@@ -128,27 +127,27 @@
 											</div><!--.row-->
 										</div>
 									</div>
-									<!--.item
+									<!--.item -->
 									<div class="item">
-										<div class="row">
-											<div class="case-landing landing1">
-												<div class="case-landing__overlay">
-													<h2 class="case-landing__title">Apex Legends</h2>
+									<div class="row rowDashboard">
+												<div class="case-landing landing2" style="background-image: url('https://duniaesports.org/wp-content/uploads/2019/12/t_5dd64b08483f2.jpg');">
+													<div class="case-landing__overlay">
+													   <h2 class="case-landing__title">Mobile Legend Squad Yogyakarta</h2>
+													</div>
 												</div>
-											</div>
-											<div class="case-landing landing1">
-												<div class="case-landing__overlay">
-													<h2 class="case-landing__title">Apex Legends</h2>
+												<div class="case-landing landing2" style="background-image: url('https://id-test-11.slatic.net/p/88cd68df4f9451f7febafdc3543459e6.png');">
+													<div class="case-landing__overlay">
+													   <h2 class="case-landing__title">Free Fire Kualifikasi Daerah</h2>
+													</div>
 												</div>
-											</div>
-											<div class="case-landing landing1">
-												<div class="case-landing__overlay">
-													<h2 class="case-landing__title">Apex Legends</h2>
+												<div class="case-landing landing2" style="background-image: url('https://pbs.twimg.com/profile_images/1083705443132690433/6j9H0Dxl_400x400.jpg');">
+													<div class="case-landing__overlay">
+													   <h2 class="case-landing__title">PUBG Nasional Invitation</h2>
+													</div>
 												</div>
-											</div>
-										</div>
+
+											</div><!--.row-->
 									</div>
-									-->
 								</div>
 							</div>
 						</div>
