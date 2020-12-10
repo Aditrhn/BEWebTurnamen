6fae23bfdba958cd9008c5bf3899b4d12dc5cc01
WahidinAji <a17wahidin@gmail.com>
2020-12-10 21:47:38 +0700

    add migration -> add table for group stage and column of knockout sistem

---
+++ database/migrations/2020_12_10_143648_create_group_matches_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_matches', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date')->nullable();
+            $table->unsignedInteger('event_id');
+            $table->unsignedInteger('group_number');
+            $table->unsignedInteger('bracket_number');
+            $table->unsignedInteger('round_number');
+            $table->unsignedInteger('match_number');
+            $table->unsignedInteger('team_a')->nullable();
+            $table->unsignedInteger('team_b')->nullable();
+            $table->unsignedInteger('score_a')->nullable();
+            $table->unsignedInteger('score_b')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_matches');
+    }
+}
